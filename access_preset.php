0c4b1f14b973226305326155166183d8ae29334c
KingdomVision <info@kingdom-vision.com>
2019-05-22 11:57:33 +0500

    Revert "updated"
    This reverts commit b234c15dcf41fa6f15b1d11bae6ecf2f2f3328a6.

---
+++ access_preset.php
@@ -0,0 +1,15 @@
+<!-- Header Section -->
+<?php require 'header.php'; ?>
+
+<!-- Sidebar Nav -->
+<?php require 'sidebar_nav.php'; ?>
+
+<!-- partial -->
+<div class="main-panel">
+  <div class="content-wrapper">
+
+</div>
+  <!-- content-wrapper ends -->
+
+<!-- Footer Section -->
+<?php require 'footer.php'; ?>
